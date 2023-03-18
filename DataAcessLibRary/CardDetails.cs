905d4aa923ce0f6908dd0c1838a4e65355424a48
raredonzen12 <84237721+raredonzen12@users.noreply.github.com>
2023-03-18 15:38:09 +0530

    add

--- DataAcessLibRary/CardDetails.cs
+++ DataAcessLibRary/CardDetails.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataAcessLibRary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,16 @@ namespace DataAcessLibRary
 {
     internal class CardDetails
     {
+        private readonly ISQLDataAcess db;
+
+        public CardDetails(ISQLDataAcess db)
+        {
+            this.db = db;
+        }
+
+        public Task<List<CardModel>> GetCards()
+        {
+            string sql = "select * from dbo.cardDetailes";
+        }
     }
 }
